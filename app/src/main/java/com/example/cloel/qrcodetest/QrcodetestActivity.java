f3754710a246124502ca3262c0072498508062ae
amelie.chambon <am.chambon@hotmail.com>
2019-02-05 12:25:25 +0100

    Rajout des demandes de permissions d'accès à la caméra

--- app/src/main/java/com/example/cloel/qrcodetest/QrcodetestActivity.java
+++ app/src/main/java/com/example/cloel/qrcodetest/QrcodetestActivity.java
@@ -1,8 +1,12 @@
 package com.example.cloel.qrcodetest;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +19,8 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QrcodetestActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
+    private static final int MY_PERMISSIONS_CAMERA = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,34 @@ public class QrcodetestActivity extends AppCompatActivity implements ZXingScanne
 
         String texte = getPreferences(Context.MODE_PRIVATE).getString("dernier", "---");
         ((TextView) findViewById(R.id.textViewResult)).setText(texte);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+
 
     }
 
